b7d01358af03983ea018a250026f49da3ab631fa
Zoolian <gamebwoy13@gmail.com>
2022-09-08 22:20:04 +0000

    update of 4 n 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - main method will print \"with proper grammar, but the outcome is a piece of art,\" on the consol
+ * main - main method will print text to the consol
  * Return: the program will return 0 if run successful
 */
 int main(void)
