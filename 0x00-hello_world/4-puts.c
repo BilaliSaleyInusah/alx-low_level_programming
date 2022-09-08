0706849b448c438ce89522cf0cbc7e7176f20426
Zoolian <gamebwoy13@gmail.com>
2022-09-08 22:09:47 +0000

    adding the rest files

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - print \"Programming is like building a multilingual puzzle on consol
+ * Return: it will return 0;
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
