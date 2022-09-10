44d8b54f8e1eaf2ba9aec947a9c3e1071ab4f8c4
Zoolian <gamebwoy13@gmail.com>
2022-09-10 16:53:30 +0000

    addvance adding

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - is used to print numbers from 0 to 9
+ * Return: return 0
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n <= 57; n++)
+	{
+		for (m = 49; m <= 57; m++)
+		{
+			if (n >= m)
+				continue;
+			putchar(n);
+			putchar(m);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
