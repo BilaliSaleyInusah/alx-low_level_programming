f0ee4fbaedd128411ac0a3c6c10fbbc41475e1be
Zoolian <gamebwoy13@gmail.com>
2022-09-22 20:34:59 +0000

    adding fie 3

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strncat - to copy string
+ * @dest: the destination string
+ * @src: the source
+ * @n: number of byte at most to be coppied
+ * Return: dest pointer
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+		dest[size] = '\0';
+	return (dest);
+}
