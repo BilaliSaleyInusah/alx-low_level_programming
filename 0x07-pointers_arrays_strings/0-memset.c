73ab57a8b1b08aca69dd0dde2b282858b8cdfe71
Zoolian <gamebwoy13@gmail.com>
2022-09-26 22:54:43 +0000

    adding first 3

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -15,7 +15,7 @@ char *_memset(char *s, char b, unsigned int n)
 
 	for (j = 0; j < n; j++)
 	{
-		s[i] = b;
+		s[j] = b;
 	}
 	return (s);
 }
--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -17,7 +17,7 @@ char *_strchr(char *s, char c)
 	for (i = 0; i < length; i++)
 	{
 		if (*s[i] == c)
-			return (s[i]);
+			return (s[i-1]);
 	}
 	return (NULL)
 }
