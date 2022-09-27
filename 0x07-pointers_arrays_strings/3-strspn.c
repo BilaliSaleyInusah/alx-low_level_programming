32c06dba7e3371ff0f5c4444afe4376660d3d3e6
Zoolian <gamebwoy13@gmail.com>
2022-09-27 00:31:23 +0000

    corrected

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,21 +1,23 @@
-#include <stdio.h>
-#include <string.h>
 #include "main.h"
+
 /**
- * _strspn - used to get the number of bytes in the initial segment
- * @s: string
- * @accept: number of bytes
- * Return: pointer to the char or nul
+ * *_strspn - gets the length of a prefix substring
+ * @s: string to evaluate
+ * @accept: string containing the list of characters to match in s
+ *
+ * Return: the number of bytes in the initial segment
+ * of s which consist only of bytes from accept
  */
-unsigned int *_strspn(char *s, char accept)
+unsigned int _strspn(char *s, char *accept)
 {
 	int i, j, f, flag;
 
 	f = 0;
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		flag = 0;
-		for (j = 0; *accept[j] != '\0'; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
@@ -28,5 +30,6 @@ unsigned int *_strspn(char *s, char accept)
 			return (f);
 		}
 	}
+
 	return (0);
 }
--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,13 +1,15 @@
-#include <stdio.h>
-#include <string.h>
 #include "main.h"
+#include <stdio.h>
+
 /**
- * _strpbrk - used to locate the first occurrence of a string in accepted
- * @s: the string
- * @accept: the accepted character
- * Return: pointer to the char or nul
+ * *_strpbrk - searches a string for any of a set of bytes
+ * @s: string to search
+ * @accept: stringcontaining the bytes to look for
+ *
+ * Return: pointer to the byte in s that matches one of the bytes in accept
+ * or NULL if no such byte is found
  */
-char *_strpbrk(char *s, char c)
+char *_strpbrk(char *s, char *accept)
 {
 	int i, j;
 
@@ -15,12 +17,13 @@ char *_strpbrk(char *s, char c)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (*s == accept[j]);
+			if (*s == accept[j])
 			{
-				return (*s);
+				return (s);
 			}
 		}
 		s++;
 	}
+
 	return (NULL);
 }
