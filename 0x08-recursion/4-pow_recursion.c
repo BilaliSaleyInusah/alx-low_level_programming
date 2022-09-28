38935d902f93253d4d9debe4cea6ecf008629214
Zoolian <gamebwoy13@gmail.com>
2022-09-28 21:44:16 +0000

    4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,15 +2,15 @@
 #include "main.h"
 /**
  * _pow_recursion - calculate a factorial num for num
- * @n: the base number
+ * @x: the base number
  * @y: the exponent number
  * Return: x^y
  */
-int _pow_recursion(int n, int y)
+int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
-	if (n == 0)
+	if (y == 0)
 		return (1);
-	return (n * _pow_recursion(n, y - 1));
+	return (x * _pow_recursion(x, y - 1));
 }
