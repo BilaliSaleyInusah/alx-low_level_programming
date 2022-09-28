d7823ca4efa9d7c2bb39755deb423eb91836c514
Zoolian <gamebwoy13@gmail.com>
2022-09-28 19:27:01 +0000

    adding the first 3

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * _puts_recursion - printing a string newline at the end
+ * @s: the string
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return (0);
+	}
+	_putchar(*s)
+	_puts_recursion(*s + 1);
+}
---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * _strlen_recursion - return the length of a string
+ * @s: the string
+ * Return: lenth of the string
+ */
+void _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0)
+	}
+	return (1 + _strlen_recursion(*s + 1);
+}
